af0f0fe51abf7b2505de4c47f3c0c326acda84f0
cdans <ch.dansard@gmail.com>
2024-09-05 12:41:16 +0800

    Initial Commit

---
+++ Tic Tac Chess macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Tic Tac Chess macOS
+//
+//  Created by Christoph Dansard on 05.09.24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
